fc18f2b78875859f0e9f1f03154d6ed654b8d134
dr-skyler <159298079+dr-skyler@users.noreply.github.com>
2024-04-18 20:10:56 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -23,7 +23,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_um_dr_skyler_clock (
+  tt_um_dr_skyler_clock user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
